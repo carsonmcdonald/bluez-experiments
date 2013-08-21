303a86f84e7b596f796dfce07a72baca50eed49e
Carson McDonald <carson@ioncannon.net>
2013-08-21 19:11:52 -0400

    Hacking away

--- experiments/scantest.c
+++ experiments/scantest.c
@@ -22,33 +22,10 @@ struct hci_state {
   char error_message[1024];
 } hci_state;
 
-#define EIR_NAME_SHORT              0x08  /* shortened local name */
-#define EIR_NAME_COMPLETE           0x09  /* complete local name */
-
-static void eir_parse_name(uint8_t *eir, size_t eir_len, char **buf)
-{
-  size_t data_len = eir[0];
-
-  if(data_len + 1 > eir_len)
-  {
-    fprintf(stderr, "EIR data length is longer than EIR packet length. %d + 1 > %d", data_len, eir_len);
-    return;
-  }
-
-  if(eir[1] == EIR_NAME_SHORT || eir[1] == EIR_NAME_COMPLETE)
-  {
-    size_t name_len = data_len - 1;
-    *buf = malloc(name_len + 1);
-    memset(*buf, 0, name_len + 1);
-    memcpy(*buf, &eir[2], name_len);
-
-    printw("Test: %s and %d\n", *buf, name_len);
-  }
-  else
-  {
-    printw("EIR not name type: %x\n", eir[1]);
-  }
-}
+#define EIR_FLAGS                   0X01
+#define EIR_NAME_SHORT              0x08
+#define EIR_NAME_COMPLETE           0x09
+#define EIR_MANUFACTURE_SPECIFIC    0xFF
 
 struct hci_state open_default_hci_device()
 {
@@ -81,14 +58,14 @@ void start_hci_scan(struct hci_state current_hci_state)
 {
   if(hci_le_set_scan_parameters(current_hci_state.device_handle, 0x01, htobs(0x0010), htobs(0x0010), 0x00, 0x00, 1000) < 0) 
   {
-    current_hci_state.state = TRUE;
+    current_hci_state.has_error = TRUE;
     snprintf(current_hci_state.error_message, sizeof(current_hci_state.error_message), "Failed to set scan parameters: %s", strerror(errno));
     return;
   }
 
   if(hci_le_set_scan_enable(current_hci_state.device_handle, 0x01, 1, 1000) < 0) 
   {
-    current_hci_state.state = TRUE;
+    current_hci_state.has_error = TRUE;
     snprintf(current_hci_state.error_message, sizeof(current_hci_state.error_message), "Failed to enable scan: %s", strerror(errno));
     return;
   }
@@ -99,7 +76,7 @@ void start_hci_scan(struct hci_state current_hci_state)
   socklen_t olen = sizeof(current_hci_state.original_filter);
   if(getsockopt(current_hci_state.device_handle, SOL_HCI, HCI_FILTER, &current_hci_state.original_filter, &olen) < 0) 
   {
-    current_hci_state.state = TRUE;
+    current_hci_state.has_error = TRUE;
     snprintf(current_hci_state.error_message, sizeof(current_hci_state.error_message), "Could not get socket options: %s", strerror(errno));
     return;
   }
@@ -113,12 +90,37 @@ void start_hci_scan(struct hci_state current_hci_state)
 
   if(setsockopt(current_hci_state.device_handle, SOL_HCI, HCI_FILTER, &new_filter, sizeof(new_filter)) < 0) 
   {
-    current_hci_state.state = TRUE;
+    current_hci_state.has_error = TRUE;
     snprintf(current_hci_state.error_message, sizeof(current_hci_state.error_message), "Could not set socket options: %s", strerror(errno));
     return;
   }
 
   current_hci_state.state = HCI_STATE_FILTERING;
+}
+
+void stop_hci_scan(struct hci_state current_hci_state)
+{
+  if(current_hci_state.state == HCI_STATE_FILTERING)
+  {
+    current_hci_state.state = HCI_STATE_SCANNING;
+    setsockopt(current_hci_state.device_handle, SOL_HCI, HCI_FILTER, &current_hci_state.original_filter, sizeof(current_hci_state.original_filter));
+  }
+
+  if(hci_le_set_scan_enable(current_hci_state.device_handle, 0x00, 1, 1000) < 0) 
+  {
+    current_hci_state.has_error = TRUE;
+    snprintf(current_hci_state.error_message, sizeof(current_hci_state.error_message), "Disable scan failed: %s", strerror(errno));
+  }
+
+  current_hci_state.state = HCI_STATE_OPEN;
+} 
+
+void close_hci_device(struct hci_state current_hci_state)
+{
+  if(current_hci_state.state == HCI_STATE_OPEN)
+  {
+    hci_close_dev(current_hci_state.device_handle);
+  }
 }
 
 void error_check_and_exit(struct hci_state current_hci_state)
@@ -183,16 +185,46 @@ void main(void)
 
       if (meta->subevent != EVT_LE_ADVERTISING_REPORT)
       {
-        break;
+        continue;
       }
 
+/*
+ *   uint8_t   evt_type;
+ *     uint8_t   bdaddr_type;
+ *       bdaddr_t  bdaddr;
+ *         uint8_t   length;
+ *           uint8_t   data[0];
+ * */
       le_advertising_info *info = (le_advertising_info *) (meta->data + 1);
 
-      char *name = NULL;
-      eir_parse_name(info->data, info->length, &name);
+      printw("Event: %d\n", info->evt_type);
+      printw("Length: %d\n", info->length);
 
-      if(name)
+      if(info->length == 0)
       {
+        continue;
+      }
+
+      size_t data_len;
+      int current_index = 0;
+
+// TODO this is just temporary
+keepgoing:
+      data_len = info->data[current_index];
+
+      if(data_len + 1 > info->length)
+      {
+        printw("EIR data length is longer than EIR packet length. %d + 1 > %d", data_len, info->length);
+        continue;
+      }
+
+      if(info->data[current_index + 1] == EIR_NAME_SHORT || info->data[current_index + 1] == EIR_NAME_COMPLETE)
+      {
+        size_t name_len = data_len - 1;
+        char *name = malloc(name_len + 1);
+        memset(name, 0, name_len + 1);
+        memcpy(name, &info->data[current_index + 2], name_len);
+
         char addr[18];
         ba2str(&info->bdaddr, addr);
 
@@ -200,30 +232,45 @@ void main(void)
 
         free(name);
       }
+      else if(info->data[current_index + 1] == EIR_FLAGS)
+      {
+        printw("Flag type: len=%d\n", data_len);
+        int i;
+        for(i=1; i<data_len; i++)
+        {
+          printw("\tFlag data: 0x%0X\n", info->data[current_index + 1 + i]);
+        }
+        current_index = current_index + 1 + i;
+        goto keepgoing; // TODO hack for now
+      }
+      else if(info->data[current_index + 1] == EIR_MANUFACTURE_SPECIFIC)
+      {
+        printw("Manufacture specific type: len=%d\n", data_len);
+        // TODO int company_id = info->data[current_index + 2] 
+        int i;
+        for(i=1; i<data_len; i++)
+        {
+          printw("\tData: 0x%0X\n", info->data[current_index + 1 + i]);
+        }
+        current_index = current_index + 1 + i;
+      }
+      else
+      {
+        printw("Unknown type: type=%X\n", info->data[current_index + 1]);
+      }
     }
-  }
-
-  if(current_hci_state.state == HCI_STATE_FILTERING)
-  {
-    current_hci_state.state = HCI_STATE_SCANNING;
-    setsockopt(current_hci_state.device_handle, SOL_HCI, HCI_FILTER, &current_hci_state.original_filter, sizeof(current_hci_state.original_filter));
   }
 
   if(error)
   {
-    perror("Could not receive advertising events");
-    endwin();
-    exit(1);
+    printw("Error scanning.");
   }
 
-  if(hci_le_set_scan_enable(current_hci_state.device_handle, 0x00, 1, 1000) < 0) 
-  {
-    perror("Disable scan failed");
-    endwin();
-    exit(1);
-  }
- 
-  hci_close_dev(current_hci_state.device_handle);
+  stop_hci_scan(current_hci_state);
+
+  error_check_and_exit(current_hci_state);
+
+  close_hci_device(current_hci_state);
 
   endwin();
 }
